875ad9af02ddcac5e8d72ee5e6c6f0cc3a6323c7
Nazar Mambetov <voldezar@gmail.com>
2017-11-10 17:32:56 +0300

    Add basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
